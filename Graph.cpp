ab3fca447d16329c6cd190d3068eb9add7123c67
ofarino <ofarino@ufl.edu>
2025-04-08 16:30:54 -0400

    Files, cmake, beginning of main

---
+++ Graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Olivia Farino on 4/8/25.
+//
+
+#include "Graph.h"
---
+++ Graph.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#ifndef GRAPH_H
+#define GRAPH_H
+
+
+
+class Graph {
+
+};
+
+
+
+#endif //GRAPH_H
