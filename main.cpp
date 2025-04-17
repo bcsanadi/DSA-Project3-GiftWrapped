c00674832ef1eb50e7c93ad7917acce65c7b8085
bcsanadi <bcsanadi01@gmail.com>
2025-04-17 16:02:47 -0400

    Added a Button and Category struct to create button options for the user. Updated main to draw the buttons on the first window.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,94 @@
 #include <sstream>
 #include "Product.h"
 #include <iostream>
+#include <optional>
 
 using namespace std;
+
+struct Button {
+    sf::RectangleShape buttonBox;
+    sf::Text label;
+    bool selected = false;
+
+    Button(const string& text, const sf::Font& font, const sf::Vector2f size, sf::Vector2f position) : label(font){
+        buttonBox.setSize(size);
+        buttonBox.setPosition(position);
+        buttonBox.setFillColor(sf::Color::White);
+        buttonBox.setOutlineColor(sf::Color::Black);
+        buttonBox.setOutlineThickness(3);
+
+        label.setFont(font);
+        label.setString(text);
+        label.setCharacterSize(15);
+        label.setFillColor(sf::Color::Black);
+        sf::FloatRect textBounds = label.getGlobalBounds();
+        label.setPosition(position + size / 2.f);
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(buttonBox);
+        window.draw(label);
+    }
+
+    bool contains(sf::Vector2f point) const {
+        return buttonBox.getGlobalBounds().contains(point);
+    }
+
+    void setSelected(bool state) {
+        selected = state;
+        if (selected) {
+            buttonBox.setFillColor(sf::Color::Black);
+        } else {
+            buttonBox.setFillColor(sf::Color::White);
+        }
+    }
+};
+
+struct Category {
+    string name;
+    vector<Button> buttons;
+    int selectedIndex = -1;
+    sf::Font font;
+    sf::Text title;
+
+    Category(const string& name, const vector<string>& values, const sf::Font& font, sf::Vector2f position) : name(name) , title(font) {
+        title.setFont(font);
+        title.setString(name);
+        title.setCharacterSize(24);
+        title.setPosition(position);
+
+        sf::Vector2f buttonSize = {150, 30};
+        float spacing = 10;
+
+        for(size_t i = 0; i < values.size(); i++) {
+            sf::Vector2f buttonPos = {position.x, position.y + 40 + i * (buttonSize.y + spacing)};
+            buttons.emplace_back(values[i], font, buttonSize, buttonPos);
+        }
+    }
+    void draw(sf::RenderWindow& window) {
+        window.draw(title);
+        for (auto& button : buttons) {
+            button.draw(window);
+        }
+    }
+
+    void handleClick(sf::Vector2f mousePos) {
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons[i].contains(mousePos)) {
+                if (selectedIndex != -1)
+                    buttons[selectedIndex].setSelected(false);
+                selectedIndex = i;
+                buttons[i].setSelected(true);
+            }
+        }
+    }
+     string getSelectedValue() const {
+        if (selectedIndex != -1) {
+            return buttons[selectedIndex].label.getString();
+        }
+        return "";
+    }
+};
 
 int main() {
     vector<string> fileNames = {
@@ -30,7 +116,17 @@ if (!font.openFromFile("../MagazineBold-3zolZ.ttf")) {
     title.setFillColor(sf::Color::White);
 
 
-    vector<Product> products;
+
+    vector<Category> categories;
+    vector<string> interests = {"Books", "Tech", "Fashion"};
+    categories.emplace_back("Interest", interests, font, sf::Vector2f(100, 150));
+    vector<string> prices = {"$0-25", "$25-50", "$50-100"};
+    categories.emplace_back("Price Range", prices, font, sf::Vector2f(300, 150));
+    vector<string> ages = {"Kids", "Teens", "Adults"};
+    categories.emplace_back("Age Group", ages, font, sf::Vector2f(500, 150));
+    vector<string> relations = {"Family", "Friend", "Partner"};
+    categories.emplace_back("Relationship", relations, font, sf::Vector2f(700, 150));
+    //vector<Product> products;
 
     // for (const auto& fileName : fileNames) {
     //     vector<Product> partial = readProductsFromFile(fileName);
@@ -41,16 +137,31 @@ if (!font.openFromFile("../MagazineBold-3zolZ.ttf")) {
 
     while (window.isOpen()){
         while (const optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
-        }
+            }
 
+            if (event->is<sf::Event::MouseButtonPressed>()) {
+            const sf::Event::MouseButtonPressed* mouseEvent = event->getIf<sf::Event::MouseButtonPressed>();
+                if (mouseEvent) {
+                    if (mouseEvent->button == sf::Mouse::Button::Left) {
+                    sf::Vector2f mousePos = window.mapPixelToCoords(mouseEvent->position);
+                        for (auto& category : categories) {
+                            category.handleClick(mousePos);
+                        }
+                    }
+                }
+            }
+        }
 
         sf::Color pink = sf::Color(255, 197, 211);
         window.clear(pink);
 
         // Draw your graph or sprites here
         window.draw(title);
+        for (auto& category : categories) {
+            category.draw(window);
+        }
         window.display();
     }
     return 0;
